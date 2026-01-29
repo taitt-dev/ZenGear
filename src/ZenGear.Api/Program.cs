2dd28f7461f3c46f3e8779dc01df380ea98525f3
taitt-dev <taitt.dev@gmail.com>
2026-01-29 20:54:32 +0700

    Add project files.

---
+++ src/ZenGear.Api/Program.cs
@@ -0,0 +1,36 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    // Scalar UI 
+    app.MapScalarApiReference();
+
+    // Swagger UI
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ZenGear API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
